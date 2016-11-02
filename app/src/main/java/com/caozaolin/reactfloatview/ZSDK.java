4f0eec1617b77de55afd041db9db596c0f9c8919
caozaolin <caozaolin@163.com>
2016-11-02 11:10:08 +0800

    提交了ReactFloatView工程代码
    这是第一个版本的ReactFloatView工程，初步仿制了酷派sdk悬浮窗的功能

---
+++ app/src/main/java/com/caozaolin/reactfloatview/ZSDK.java
@@ -0,0 +1,30 @@
+package com.caozaolin.reactfloatview;
+
+import android.app.Activity;
+
+public class ZSDK {
+
+	private static ZSDK instance;
+	private Activity context;
+
+	private ZSDK() {
+
+	}
+
+	public static ZSDK getInstance() {
+		if (instance == null) {
+			instance = new ZSDK();
+		}
+		return instance;
+	}
+
+	public void init(Activity context) {
+		this.context = context;
+	}
+
+	public Activity getContext() {
+		return this.context;
+
+	}
+
+}
---
+++ app/src/main/java/com/caozaolin/reactfloatview/services/GamePlugIdleTimeService.java
@@ -0,0 +1,54 @@
+package com.caozaolin.reactfloatview.services;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import android.os.Handler;
+
+import com.caozaolin.reactfloatview.task.BrandWindowShowTask;
+import com.caozaolin.reactfloatview.view.GameAssistWindowManager;
+
+/**
+ * Desction:该类控制了small和brand两种形态的转变
+ * Author:caozaolin
+ * Date:16/09/29
+ */
+public class GamePlugIdleTimeService {
+	public static final int DEAFAULT_IDLE_TIME = 10;
+	private ScheduledThreadPoolExecutor mGameIdleTimeExec = null;
+	private volatile GameAssistWindowManager mGameAssistWindowManager;
+	private volatile BrandWindowShowTask brandWindowShowTask = null;
+	private volatile Runnable brandWindowShowRunnable;
+
+	public GamePlugIdleTimeService(GameAssistWindowManager gameAssistWindowManager) {
+		this.mGameIdleTimeExec = new ScheduledThreadPoolExecutor(1);
+		this.mGameAssistWindowManager = gameAssistWindowManager;
+	}
+
+	/**
+	 * Desction:展示brand形态悬浮窗
+	 */
+	public void executeIdleTimeService(final Handler uiHandler) {
+		this.brandWindowShowTask = new BrandWindowShowTask();
+		this.brandWindowShowRunnable = new Runnable() {
+			@Override
+			public void run() {
+				uiHandler.post(new Runnable()
+		        {
+		          public void run() {
+		            GamePlugIdleTimeService.this.brandWindowShowTask.showBrandWindow(GamePlugIdleTimeService.this.mGameAssistWindowManager);
+		          }
+		        });
+			}
+		};
+		this.mGameIdleTimeExec.schedule(this.brandWindowShowRunnable, 10L, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Desction:关闭服务
+	 */
+	public void cancelIdleTimeService() {
+		if (!this.mGameIdleTimeExec.isShutdown())
+			this.mGameIdleTimeExec.shutdownNow();
+	}
+}
